f49ffdb4fd0622031a23f82176f9a5a45ab63c0f
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-19 04:35:15 -0800

    8th try t4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main - entry point
- * return: always (Success)
  */
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
