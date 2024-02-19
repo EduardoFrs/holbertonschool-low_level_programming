de06eca07f735ab829892da1f0ed926981260fff
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-19 04:53:51 -0800

    1st try t5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	pritnf(with proper grammar, but the outcome si a piece of art,);
+	return (0);
+}
