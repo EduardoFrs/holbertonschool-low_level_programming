1e58b42c6097fc1cb163a37576293a5ce4ca2ad3
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-19 03:59:45 -0800

    3rd try t4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* print Programming is like building a multilingual puzzle */
 int main(void)
 { 
-	puts("\"Programming il like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
-}
+} 
