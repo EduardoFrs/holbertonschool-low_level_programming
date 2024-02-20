bc804dff3164f81155778bb65c8a9767cef9f69a
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-20 03:51:02 -0800

    1st try t4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
