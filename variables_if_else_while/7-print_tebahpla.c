15571ed5065780ddd4f1c03278fb725af6aa7b75
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-20 04:22:08 -0800

    1st try t7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	for (ch = 'z'; ch <= 'a'; ch++)
+
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
