8a4f131e3a763f187585b0705e38a2c3e20d288a
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-20 04:12:47 -0800

    1st try t6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char ch = '1';
+
+	for (ch = '1'; ch <= '9'; ch++)
+
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
