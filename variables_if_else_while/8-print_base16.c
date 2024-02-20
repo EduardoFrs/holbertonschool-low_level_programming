9d08b06cd7a1848ed96f0d20e37ca6465d7d0bde
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-20 04:49:08 -0800

    1st try t8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char ch = '0';
+
+	for (ch = '0'; ch <= '9'; ch++)
+
+		putchar(ch);
+
+	{
+		char ch = 'a';
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
