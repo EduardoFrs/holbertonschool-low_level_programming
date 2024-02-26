82d7c949faf118cfc8d7fd677a8f0a880ba2beed
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-26 12:14:38 +0100

    betty t5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,7 @@ void more_numbers(void)
 		for (numbers = 0 ; numbers <= 14 ; numbers++)
 		{
 			if (numbers > 9)
-		{ 
+		{
 			_putchar((numbers / 10) + '0');
 		}
 			_putchar((numbers % 10) + '0');
