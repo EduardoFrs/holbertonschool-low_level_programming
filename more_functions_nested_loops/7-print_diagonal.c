fbd81e5ff9fa8a8123e24895ba1950f781f37a02
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-26 13:45:49 +0100

    1st try t7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal line
+ *@n: number of time to print
+ */
+
+void print_diagonal(int n)
+{
+	int diag; int space;
+
+	if (n > 0)
+	{
+		for (diag = 0; diag < n ; diag++)
+		{
+			for (space = 0; space < diag; space++)
+				_putchar(' ');
+			_putchar('\\');
+			if (diag == n - 1)
+				continue;
+		}
+	}
+	_putchar('\n');
+}
