5afb56145b15b0906aa7c97c7daf7de17e928f57
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-26 14:39:51 +0100

    1st try t8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * @size: size of the square
+ */
+
+void print_square(int size)
+
+{
+	int ligne, colonne;
+
+	if (size > 0)
+	{
+		for (ligne = 0; ligne < size; ligne++)
+		{
+			for (colonne = 0; colonne < size; colonne++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
