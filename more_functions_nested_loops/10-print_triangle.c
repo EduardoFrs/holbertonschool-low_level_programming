3b006a229cb8c2661d612e1a764993ae38d04125
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-27 11:26:12 +0100

    betty t10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,17 +2,16 @@
 
 /**
  * print_triangle - print a triangle
- * @size - size of the triangle
+ * @size: size of the triangle
  */
 
 void print_triangle(int size)
-
 {
 
 	int a, b;
 
 	if (size > 0)
-	{ 
+	{
 		for (a = 1; a <= size; a++)
 		{
 			for (b = size - a; b > 0; b--)
