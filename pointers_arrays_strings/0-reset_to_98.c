1c876dc7556519c1a0ceb5120072181257d68a6f
EduardoFrs <fernandeseduardo00@gmail.com>
2024-02-27 13:41:39 +0100

    test t0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /*
- * reset_to_98 - take a pointer as parameter and update is value to 98
+ *reset_to_98 - updates the value of the variable
  */
 
 void reset_to_98(int *n)
-
 {
-	*n = 98; 
+	*n = 98;
 }
