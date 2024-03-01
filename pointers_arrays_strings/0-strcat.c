3f1e1198ca774fcd32a6d3d8e4898cae6ec4154e
EduardoFrs <fernandeseduardo00@gmail.com>
2024-03-01 12:33:45 +0100

    1st try t0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -7,3 +7,18 @@
  * Return : pointer to the resulting string
  */
 
+char *_strcat(char *dest, char *src)
+{
+	int i, c;
+
+	for ( i = 0; dest[i] != '\0' ; i++)
+
+		for (c = 0; src[c] != '\0'; c++)
+		{
+			dest[i] = src[c];
+			i++;
+		}
+
+	dest[i] = '\0';
+	return (dest);
+}
