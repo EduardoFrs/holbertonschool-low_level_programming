792a41d96f4c372bc2829b1057b120f68990a7ea
EduardoFrs <fernandeseduardo00@gmail.com>
2024-03-01 12:04:31 +0100

    add comments

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _strcat - function that append a string to another one
+ * @src: string to be append
+ * @dest: first string
+ * Return : pointer to the resulting string
+ */
+
