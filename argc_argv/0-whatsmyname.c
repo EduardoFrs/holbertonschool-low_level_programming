883d061705c7f7dd137ac5bffd6b9f8e6a0579c3
EduardoFrs <fernandeseduardo00@gmail.com>
2024-03-07 10:09:30 +0100

    betty task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - print is name followed by a new line
+ * @argc: argument count
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	(void)argc;
