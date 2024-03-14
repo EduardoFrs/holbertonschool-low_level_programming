6becc0bd96fd1275c4ed2fb0574dd50eca48afbb
EduardoFrs <fernandeseduardo00@gmail.com>
2024-03-14 10:08:00 +0100

    add header guards

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - new type of variable describing a dog
  * @name: the name of the dog
@@ -11,3 +14,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
