086f12431ae2f92826195d92a834f05c934e74c6
EduardoFrs <fernandeseduardo00@gmail.com>
2024-03-14 09:36:41 +0100

    add comments

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,10 @@
+/**
+ * struct dog - new type of variable describing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
 struct dog
 {
 	char *name;
