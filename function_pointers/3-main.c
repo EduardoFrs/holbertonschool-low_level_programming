2177ebce20e48bb488f4c1df4f2f7c1fbbcf65ea
EduardoFrs <fernandeseduardo00@gmail.com>
2024-03-19 09:26:37 +0100

    fix and add comments

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - print the result of the operation
+ * @argc: number of argument of the program
+ * @argv: array of pointer to the arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
 	{
-	int (*oprt)(int, int);
+	int (*operator)(int, int);
 
 	if (argc != 4)
 	{
@@ -12,14 +20,14 @@ int main (int argc, char *argv[])
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	operator = get_op_func(argv[2]);
 
-	if (!oprt)
+	if (operator == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", operator(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
