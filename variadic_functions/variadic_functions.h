8e68db0d361a472ab2f5828e2956f76dbce1b95d
EduardoFrs <fernandeseduardo00@gmail.com>
2024-03-20 12:34:01 +0100

    add new struc on the header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,12 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+typedef struct print_type
+{
+        char letter;
+        void (*f)(va_list args);
+} print_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
