e1d4e67e31bfab709c2e54f3748ea4858048046d
EduardoFrs <fernandeseduardo00@gmail.com>
2024-04-03 11:12:24 +0200

    add header with update prototype

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -11,6 +11,7 @@
  * Description: doubly linked list node structure
  * 
  */
+
 typedef struct dlistint_s
 {
     int n;
@@ -19,5 +20,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
